786096515c036e39102acf0be2729feb6c9b2768
subham18 <subham.dandapat@memeinfotech.com>
2020-01-23 23:34:23 +0530

    first commit

--- home/src/main/java/notification/NotificationService.java
+++ home/src/main/java/notification/NotificationService.java
@@ -9,14 +9,24 @@ import android.util.Log;
 import android.support.v4.content.LocalBroadcastManager;
 import android.app.Notification;
 import modals.Action;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.robj.notificationhelperlibrary.utils.NotificationUtils;
 import android.app.PendingIntent;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import notification.NotificationHolder;
 
 public class NotificationService extends NotificationListenerService {
 
     Context context;
-
+    String reply = "";
+    Action action;
     @Override
 
     public void onCreate() {
@@ -35,11 +45,11 @@ public class NotificationService extends NotificationListenerService {
         Bundle extras = sbn.getNotification().extras;
         String title = extras.getString("android.title");
         String text = extras.getCharSequence("android.text").toString();
-        String reply = "";
+//        String reply = "";
 
         if(pack.equals("com.whatsapp")){
             NotificationService.this.cancelNotification(sbn.getKey());
-            Action action = NotificationUtils.getQuickReplyAction(sbn.getNotification(), sbn.getPackageName());
+            action = NotificationUtils.getQuickReplyAction(sbn.getNotification(), sbn.getPackageName());
             Intent msgrcv = new Intent("Msg");
             msgrcv.putExtra("package", pack);
             msgrcv.putExtra("ticker", ticker);
@@ -49,11 +59,38 @@ public class NotificationService extends NotificationListenerService {
 
             // make a api call here
 
+            JSONObject json = new JSONObject();
+            try{
+                json.put("name","morpheus");
+                json.put("job","leader");
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+            String url = " https://reqres.in/api/users";
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+                    new Response.Listener <JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println("responce------->>>>>>"+response.toString());
+                            try {
+                                JSONObject serverResp = new JSONObject(response.toString());
+                                reply = "Hey ! Happy New Year";
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    reply = "Hey, I am busy now. Ping me back after some time or call Pushpendu on +917047434141 if this is urgent";
+                    reply(action,context,reply);
+                }
+            });
             // on error
-            reply = "Hey, I am busy now. Ping me back after some time or call Pushpendu on +917047434141 if this is urgent";
-            reply(action,context,reply);
+//            reply = "Hey, I am busy now. Ping me back after some time or call Pushpendu on +917047434141 if this is urgent";
+//            reply(action,context,reply);
             // on success
-            reply = "Hey ! Happy New Year";
+//            reply = "Hey ! Happy New Year";
 
         }
 
